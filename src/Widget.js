b630e20c30bf084ce408f9948aea5deb574c0f6f
Nick Gromyko <nxgr.dev@gmail.com>
2024-03-22 21:43:22 +0500

    rework markup from js to template strings

--- src/Widget.js
+++ src/Widget.js
@@ -1,4 +1,4 @@
-import Icons from "./Icons.js"
+import markup from "./markup.js"
 
 class Widget {
     constructor(container) {
@@ -8,47 +8,28 @@ class Widget {
     }
 
     #initializeWidget() {
-        const activator = document.createElement("div")
-        activator.classList.add("relay-activator")
+        this.container.innerHTML = markup
 
-        const chatContainer =  document.createElement("div")
-        chatContainer.classList.add("relay-chat-container")
-
-        const button = document.createElement("div")
-        button.appendChild(createSvg(Icons.mdiClose))
-        button.setAttribute("role", "button")
-        button.classList.add("relay-close-btn")
-
-        button.addEventListener("click", () => {
-            if (chatContainer.classList.contains("active")) {
-                chatContainer.classList.remove("active")
-            }
-        })
-
-        chatContainer.appendChild(button)
-
-        activator.addEventListener("click", () => {
-            if (!chatContainer.classList.contains("active")) {
-                chatContainer.classList.add("active")
-            }
-        })
-
-        this.container.append(activator)
-        this.container.append(chatContainer)
-        console.log('initialized')
+        bindEventListeners(this.container)
     }
 }
 
-function createSvg(path, viewBox = "0 0 24 24") {
-    const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg")
-    svg.setAttribute("viewBox", viewBox)
-    svg.setAttribute("fill", "currentColor")
+function bindEventListeners(container) {
+    const chatActivator = container.querySelector(".relay-activator"),
+        chatContainer = container.querySelector(".relay-chat-container"),
+        closeChatBtn = container.querySelector('.relay-close-btn')
 
-    const pathElement = document.createElementNS("http://www.w3.org/2000/svg", "path")
-    pathElement.setAttribute("d", path)
-    svg.appendChild(pathElement);
+    chatActivator.addEventListener("click", () => {
+        if (!chatContainer.classList.contains("active")) {
+            chatContainer.classList.add("active")
+        }
+    })
 
-    return svg
+    closeChatBtn.addEventListener("click", () => {
+        if (chatContainer.classList.contains("active")) {
+            chatContainer.classList.remove("active")
+        }
+    })
 }
 
 export default Widget
