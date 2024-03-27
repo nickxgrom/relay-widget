861b3eb29101d7cc2478788a6124688437969b21
Nick Gromyko <nxgr.dev@gmail.com>
2024-03-27 23:12:39 +0500

    add websocket backend integration (sketch)

--- src/Widget.js
+++ src/Widget.js
@@ -1,7 +1,7 @@
 import markup from "./markup.js"
 
 const baseUrl = "http://localhost:3000",
-    wsUrl = ""
+    wsUrl = "ws://localhost:3001"
 
 class Widget {
     constructor(container, organizationId) {
@@ -11,9 +11,21 @@ class Widget {
         this.#initializeWidget()
     }
 
-    #initializeWidget() {
-        const cookies = document.cookie.split(';');
-        const relayChatToken = cookies.find((item) => item.trim().startsWith('relay-token='));
+    #connectToWebSocket() {
+        this.wsConnection = new WebSocket(wsUrl + `?relay-token=${localStorage.getItem('relay-chat-token')}`);
+
+        this.wsConnection.onopen = function(e) {
+            console.log("[open] Connection established")
+        };
+
+        this.wsConnection.onerror = function(error) {
+            console.log(`[error]`, error)
+        };
+
+        this.wsConnection.onmessage = async (event) => {
+            console.log(`[message] Data received from server: ${event.data}`)
+
+            const msg = JSON.parse(event.data ?? "")
 
         if (relayChatToken) {
             const tokenValue = relayChatToken.split('=')[1];
@@ -22,35 +34,54 @@ class Widget {
             createChat(this.organizationId)
         }
 
+            if (msg.type === "history") {
+                // TODO
+            }
+
+            if (msg.type === "message") {
+                console.log(msg)
+                // TODO
+            }
+         }
+    }
+
+    async #initializeWidget() {
         this.container.innerHTML = markup
 
-        bindEventListeners(this.container)
+        this.#bindEventListeners()
+    }
+
+    #bindEventListeners() {
+        const chatActivator = this.container.querySelector(".relay-activator"),
+            chatContainer = this.container.querySelector(".relay-chat-container"),
+            closeChatBtn = this.container.querySelector('.relay-close-btn')
+
+        chatActivator.addEventListener("click", async () => {
+            if (!chatContainer.classList.contains("active")) {
+                const token = localStorage.getItem('relay-chat-token')
+
+                if (!token) {
+                    const {token} = await createChat(this.organizationId)
+
+                    localStorage.setItem("relay-chat-token", token)
+                }
+
+                this.#connectToWebSocket();
+                chatContainer.classList.add("active")
+            }
+        })
+
+        closeChatBtn.addEventListener("click", () => {
+            if (chatContainer.classList.contains("active")) {
+                chatContainer.classList.remove("active")
+            }
+        })
     }
 }
 
 function createChat(orgId) {
-    useFetch(`/chat/${orgId}`, {
+    return useFetch(`/chat/${orgId}`, {
         method: "POST",
-    }).then(token => {
-        console.log(token)
-    })
-}
-
-function bindEventListeners(container) {
-    const chatActivator = container.querySelector(".relay-activator"),
-        chatContainer = container.querySelector(".relay-chat-container"),
-        closeChatBtn = container.querySelector('.relay-close-btn')
-
-    chatActivator.addEventListener("click", () => {
-        if (!chatContainer.classList.contains("active")) {
-            chatContainer.classList.add("active")
-        }
-    })
-
-    closeChatBtn.addEventListener("click", () => {
-        if (chatContainer.classList.contains("active")) {
-            chatContainer.classList.remove("active")
-        }
     })
 }
 
--- src/index.js
+++ src/index.js
@@ -2,4 +2,6 @@ import Widget from "./Widget.js"
 
 const container = document.getElementById("relay-chat")
 
-const widget = new Widget(container, "e91ac276-5d6c-49c1-89b1-8255c6cda945")
+window.addEventListener('load',() => {
+    const widget = new Widget(container, "e91ac276-5d6c-49c1-89b1-8255c6cda945")
+})
